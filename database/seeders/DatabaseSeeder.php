079eebbd62a3673ae4eb3071e48cc741e14d9147
Flavien Chagras <flavien.chagras@protonmail.com>
2022-01-30 17:43:29 +0100

    feat(app): DB Seeder added
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Menu;
+use App\Models\Product;
+use App\Models\Restaurant;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call('UsersTableSeeder');
+    public function run() {
+        for ($i = 0; $i < 5; $i++) {
+            $restaurant = Restaurant::factory()->create();
+            $products = Product::factory(10)->create();
+            foreach ($products as $product) {
+                $product->restaurants()->attach($restaurant);
+                $menu = Menu::factory()->create();
+                $menu->restaurants()->attach($restaurant);
+                $product->menus()->attach($menu);
+            }
+
+            $products = Product::factory(10)->create();
+            foreach ($products as $product) {
+                $product->restaurants()->attach($restaurant);
+            }
+        }
     }
 }
