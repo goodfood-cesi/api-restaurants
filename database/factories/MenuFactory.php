2daf5321ff872fd8e287620813fa15c524897f85
Flavien Chagras <flavien.chagras@protonmail.com>
2022-01-30 17:42:58 +0100

    feat(app): Restaurant Factory added
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Menu;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MenuFactory extends Factory {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Menu::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'amount' => $this->faker->randomFloat(2,0,1000),
+            'description' => $this->faker->text(),
+        ];
+    }
+}
