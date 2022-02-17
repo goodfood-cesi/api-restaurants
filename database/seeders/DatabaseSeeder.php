dccbfd486a11b77314a5f2f5392a908c678a42ed
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-17 20:20:31 +0100

    feat(app): more realistic seeder
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,16 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        for ($i = 0; $i < 100; $i++) {
+        for ($i = 0; $i < 1; $i++) {
             $restaurant = Restaurant::factory()->create();
-            $products = Product::factory(10)->create();
-            foreach ($products as $product) {
-                $product->restaurants()->attach($restaurant);
+            $products = Product::factory(15)->create();
+            for($j = 0; $j < 5; $j++){
                 $menu = Menu::factory()->create();
                 $menu->restaurants()->attach($restaurant);
-                $product->menus()->attach($menu);
+                foreach ($products->random(random_int(1,4)) as $product){
+                    $product->restaurants()->attach($restaurant);
+                    $product->menus()->attach($menu, ['quantity' => random_int(1,3)]);
+                }
             }
 
             $products = Product::factory(10)->create();
