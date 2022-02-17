1a863dc8e080699f1b77be3205bb098e142a272f
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-17 20:21:12 +0100

    feat(app): update seeder
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder {
      * @return void
      */
     public function run() {
-        for ($i = 0; $i < 1; $i++) {
+        for ($i = 0; $i < 20; $i++) {
             $restaurant = Restaurant::factory()->create();
             $products = Product::factory(15)->create();
             for($j = 0; $j < 5; $j++){
