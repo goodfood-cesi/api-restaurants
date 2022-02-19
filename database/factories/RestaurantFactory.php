548960a88c6811b56a747a1762f44a0e8cc58ee8
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-19 16:20:24 +0100

    feat(app): add days in restaurant factory
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -18,15 +18,21 @@ class RestaurantFactory extends Factory {
      *
      * @return array
      */
-    public function definition()
-    {
+    public function definition() {
         return [
             'name' => $this->faker->name,
             'image' => 'https://source.unsplash.com/random/2160x720/?restaurants',
             'address' => $this->faker->address,
-            'latitude' => $this->faker->latitude(36.1426653,59.3696544),
-            'longitude' => $this->faker->longitude(-5.4716872,28.0401234),
-            'phone' => $this->faker->phoneNumber
+            'latitude' => $this->faker->latitude(36.1426653, 59.3696544),
+            'longitude' => $this->faker->longitude(-5.4716872, 28.0401234),
+            'phone' => $this->faker->phoneNumber,
+            'monday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'tuesday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'wednesday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'thursday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'friday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'saturday' => "11h00 - 14h00 / 18h00 - 22h00",
+            'sunday' => "Fermé"
         ];
     }
 }
