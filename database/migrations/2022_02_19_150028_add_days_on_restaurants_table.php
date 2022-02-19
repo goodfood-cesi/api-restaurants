326fa5600f0718ca69c5ab737c01c229fd912c0f
Flavien Chagras <flavien.chagras@protonmail.com>
2022-02-19 16:18:46 +0100

    feat(app): add days in restaurant
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

---
+++ database/migrations/2022_02_19_150028_add_days_on_restaurants_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDaysOnRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $table->string('monday')->nullable();
+            $table->string('tuesday')->nullable();
+            $table->string('wednesday')->nullable();
+            $table->string('thursday')->nullable();
+            $table->string('friday')->nullable();
+            $table->string('saturday')->nullable();
+            $table->string('sunday')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employees', function (Blueprint $table) {
+            $table->dropColumn('monday');
+            $table->dropColumn('tuesday');
+            $table->dropColumn('wednesday');
+            $table->dropColumn('thursday');
+            $table->dropColumn('friday');
+            $table->dropColumn('saturday');
+            $table->dropColumn('sunday');
+        });
+    }
+}
