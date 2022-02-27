359b10e5edff56410842485f2383761a782290ab
Jules Sayer <jules.sayer@protonmail.com>
2022-02-27 12:16:22 +0100

    feat(products): added product show

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@
 $router->get('/restaurants', ['as' => 'restaurants.index', 'uses' => 'RestaurantController@index']);
 $router->get('/restaurants/{restaurant_id}', ['as' => 'restaurants.show', 'uses' => 'RestaurantController@show']);
 $router->get('/restaurants/{restaurant_id}/products', ['as' => 'restaurants.products.index', 'uses' => 'ProductController@index']);
+$router->get('/restaurants/{restaurant_id}/products/{product_id}', ['as' => 'restaurants.products.show', 'uses' => 'ProductController@show']);
 $router->get('/restaurants/{restaurant_id}/menus', ['as' => 'restaurants.menus.index', 'uses' => 'MenuController@index']);
 $router->get('/restaurants/{restaurant_id}/menus/{menu_id}', ['as' => 'restaurants.menus.show', 'uses' => 'MenuController@show']);
 $router->get('/restaurants/{restaurant_id}/menus/{menu_id}/products', ['as' => 'restaurants.menus.products.index', 'uses' => 'ProductController@index']);
