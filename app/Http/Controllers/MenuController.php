fc4c769a50f11cb5d70854c8e1975f50a9b1bd39
Flavien Chagras <flavien.chagras@protonmail.com>
2022-03-02 22:21:32 +0100

    feat(app): CRUD menus
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\MenuResource;
 use App\Models\Restaurant;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller {
     public function index(int $restaurant_id): JsonResponse {
@@ -22,5 +24,55 @@ class MenuController extends Controller {
         } catch (Exception $e){
             return $this->error('Resource does not exist.');
         }
+    }
+
+    public function store(Request $request, int $restaurant_id): JsonResponse {
+        $input = $request->only(
+            'name',
+            'image',
+            'amount',
+            'description'
+        );
+        $validator = Validator::make($input,[
+            'name' => 'required|string',
+            'image' => 'required|string',
+            'amount' => 'required|numeric',
+            'description' => 'required|string'
+        ]);
+
+        if($validator->fails()){
+            return $this->error('Wrongs inputs', $validator->errors()->toArray(), 422);
+        }
+        $menu = Restaurant::findOrFail($restaurant_id)?->menus()->create($input);
+        return $this->ressourceCreated(new MenuResource($menu), "Menu created");
+    }
+
+    public function update(Request $request, int $restaurant_id, int $menu_id){
+        $input = $request->only([
+            'name',
+            'image',
+            'amount',
+            'description'
+        ]);
+
+        $validator = Validator::make($input,[
+            'name' => 'string',
+            'image' => 'string',
+            'amount' => 'numeric',
+            'description' => 'string'
+        ]);
+
+        if($validator->fails()){
+            return $this->error('Wrongs inputs', $validator->errors()->toArray(), 422);
+        }
+
+        $menu = Restaurant::findOrFail($restaurant_id)->menus()->findOrFail($menu_id);
+        $menu->update($input);
+        return $this->success(new MenuResource($menu), "Menu updated");
+    }
+
+    public function destroy(int $restaurant_id, int $menu_id): JsonResponse {
+        Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id)?->delete();
+        return $this->ressourceDeleted();
     }
 }
