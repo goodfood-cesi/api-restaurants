59ce498186dd22ddf5ebc0b050e22efc8928d871
Baptiste LAUR <baptiste.laur@gmail.com>
2022-06-19 20:37:14 +0200

    feat(app): Product Controller added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\ProductResource;
 use App\Models\Restaurant;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller{
     public function index(int $restaurant_id, int $menu_id = null): JsonResponse {
@@ -21,4 +23,48 @@ class ProductController extends Controller{
             return $this->error('Resource does not exist.');
         }
     }
+<<<<<<< Updated upstream
+=======
+
+    public function show(int $restaurant_id, int $product_id): JsonResponse {
+        return $this->success(new ProductResource(
+            Restaurant::findOrFail($restaurant_id)->products()->findOrFail($product_id)
+        ), 'Products loaded');
+    }
+
+    public function destroy(int $restaurant_id, int $product_id): JsonResponse {
+        try {
+            return $this->success((Restaurant::findOrFail($restaurant_id)->products()->findOrFail($product_id))->delete(), 'Product deleted');
+        } catch (Exception $e){
+            return $this->error($e->getMessage());
+        }
+    }
+
+    public function update(Request $request, int $restaurant_id, int $product_id): JsonResponse {
+        try {
+            $product = Restaurant::findOrFail($restaurant_id)?->products()->findOrFail($product_id);
+
+            $validator = Validator::make($request->all(),[
+                'name' => 'required|string',
+                'image' => 'required|string',
+                'amount' => 'required|between:0,99.99',
+                'description' => 'required|string',
+            ]);
+
+            if($validator->fails()){
+                return $this->error('Wrong informations');    
+            }
+
+            $product->name = $request->input('name');
+            $product->image = $request->input('image'); 
+            $product->amount = $request->input('amount'); 
+            $product->description = $request->input('description');
+            $product->save();
+
+            return $this->success('Product updated');
+        } catch (Exception $e){
+            return $this->error($e->getMessage());
+        }
+    }
+>>>>>>> Stashed changes
 }
