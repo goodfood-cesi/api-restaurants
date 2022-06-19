18e8f25475eb641282f277c9eec2e414024acb13
Baptiste LAUR <baptiste.laur@gmail.com>
2022-06-19 20:37:04 +0200

    feat(app): Menus Controller added

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\MenuResource;
 use App\Models\Restaurant;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MenuController extends Controller {
     public function index(int $restaurant_id): JsonResponse {
@@ -21,6 +23,41 @@ class MenuController extends Controller {
             return $this->success(new MenuResource(Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id)), 'Menu fetched');
         } catch (Exception $e){
             return $this->error('Resource does not exist.');
+        }
+    }
+
+    public function destroy(int $restaurant_id, int $menu_id): JsonResponse {
+        try {
+            return $this->success((Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id))->delete(), 'Menu deleted');
+        } catch (Exception $e){
+            return $this->error($e->getMessage());
+        }
+    }
+
+    public function update(Request $request, int $restaurant_id, int $menu_id): JsonResponse {
+        try {
+            $menu = Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id);
+
+            $validator = Validator::make($request->all(),[
+                'name' => 'required|string',
+                'image' => 'required|string',
+                'amount' => 'required|between:0,99.99',
+                'description' => 'required|string',
+            ]);
+
+            if($validator->fails()){
+                return $this->error('Wrong informations');    
+            }
+
+            $menu->name = $request->input('name');
+            $menu->image = $request->input('image'); 
+            $menu->amount = $request->input('amount'); 
+            $menu->description = $request->input('description');
+            $menu->save();
+
+            return $this->success('Menu updated');
+        } catch (Exception $e){
+            return $this->error($e->getMessage());
         }
     }
 }
