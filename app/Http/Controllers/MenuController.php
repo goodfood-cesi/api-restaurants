ce8a6da076a5371ba5f808f84fd91778baabc7da
Flavien Chagras <flavien.chagras@protonmail.com>
2022-06-27 23:18:19 +0200

    feat(app): menu create and upload img added
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\MenuResource;
 use App\Models\Restaurant;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class MenuController extends Controller {
     public function index(int $restaurant_id): JsonResponse {
@@ -15,15 +16,36 @@ class MenuController extends Controller {
     public function show(int $restaurant_id, int $menu_id): JsonResponse {
         return $this->success(new MenuResource(Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id)), 'Menu fetched');
     }
-    public function update(Request $request, int $restaurant_id, int $menu_id): JsonResponse {
-        $menu = Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id);
 
-        $input = $request->validate($request,[
+    public function store(Request $request, int $restaurant_id): JsonResponse {
+        $input = $this->validate($request,[
             'name' => 'required|string',
-            'image' => 'required|string',
+            'image' => 'required|file|mimes:jpg,png,jpeg,gif,svg,tif,tiff,bmp,gif,xe2,webp,heic,pdf|max:5000',
             'amount' => 'required|between:0,99.99',
             'description' => 'required|string',
         ]);
+
+        $file = $request->file('image');
+        $extension = $file->extension();
+        $input['image'] = $_ENV['AWS_BUCKET_URL'] . '/' . $file->storePubliclyAs('', Str::uuid() . '.' . $extension, 's3');
+
+        $menu = Restaurant::findOrFail($restaurant_id)->menus()->create($input);
+        return $this->ressourceCreated(new MenuResource($menu), 'Menu created');
+    }
+
+    public function update(Request $request, int $restaurant_id, int $menu_id): JsonResponse {
+        $menu = Restaurant::findOrFail($restaurant_id)?->menus()->findOrFail($menu_id);
+
+        $input = $this->validate($request,[
+            'name' => 'string',
+            'image' => 'file|mimes:jpg,png,jpeg,gif,svg,tif,tiff,bmp,gif,xe2,webp,heic,pdf|max:5000',
+            'amount' => 'between:0,99.99',
+            'description' => 'string',
+        ]);
+
+        $file = $request->file('image');
+        $extension = $file->extension();
+        $input['image'] = $_ENV['AWS_BUCKET_URL'] . '/' . $file->storePubliclyAs('', Str::uuid() . '.' . $extension, 's3');
 
         $menu->update($input);
         return $this->success(new MenuResource($menu),'Menu updated');
