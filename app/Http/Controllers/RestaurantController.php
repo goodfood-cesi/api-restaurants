562f64c94f858b73149aa76757a14a3f0ab46653
Flavien Chagras <flavien.chagras@protonmail.com>
2022-07-07 22:48:22 +0200

    fix(app): fix Restaurant Controller
    Signed-off-by: Flavien Chagras <flavien.chagras@protonmail.com>

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -21,17 +21,26 @@ class RestaurantController extends Controller{
             'name' => 'required|string',
             'image' => 'required|string',
             'address' => 'required|string',
-            'latitude' => 'required|string',
-            'longitude' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
             'phone' => 'required|string',
-            'monday' => 'nullable|string',
-            'tuesday' => 'nullable|string',
-            'wednesday' => 'nullable|string',
-            'thursday' => 'nullable|string',
-            'friday' => 'nullable|string',
-            'saturday' => 'nullable|string',
-            'sunday' => 'nullable|string',
+            "days" => 'nullable|array',
+            "days.monday" => 'nullable|string',
+            'days.tuesday' => 'nullable|string',
+            'days.wednesday' => 'nullable|string',
+            'days.thursday' => 'nullable|string',
+            'days.friday' => 'nullable|string',
+            'days.saturday' => 'nullable|string',
+            'days.sunday' => 'nullable|string',
         ]);
+
+        if(isset($input['days'])) {
+            foreach ($input['days'] as $key => $value) {
+                $input[$key] = $value;
+            }
+            unset($input['days']);
+        }
+
 
         $restaurant = Restaurant::create($input);
         return $this->ressourceCreated(new RestaurantResource($restaurant), 'Restaurant created');
